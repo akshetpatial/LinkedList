5b841e4e8be47212af3bb4f61f7973dd32aa2605
AKSHET PATIAL <akshet.patial@gmail.com>
2023-04-27 11:51:53 +0530

    Created the LinkedList project and initialized the git

---
+++ src/com/akshet/Linked_List.java
@@ -0,0 +1,10 @@
+package com.akshet;
+
+import java.util.LinkedList;
+
+public class Linked_List {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>() ;
+    }
+}
