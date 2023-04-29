c9315faf157a6348eeab8bd7cf22627926a01b4f
AKSHET PATIAL <akshet.patial@gmail.com>
2023-04-30 02:43:40 +0530

    Created the DoublyLinkedList.java class

---
+++ src/com/akshet/DoublyLinkedList.java
@@ -0,0 +1,13 @@
+package com.akshet;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DoublyLinkedList {
+
+    public static void main(String[] args) {
+        // In Java LinkedList can also be used a doubly Linked List
+        List<Integer> doubleLL= new LinkedList<>();
+
+    }
+}
