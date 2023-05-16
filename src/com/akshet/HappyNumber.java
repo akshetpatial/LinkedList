6e819b8a65ea2c90d66296fab2a9492144e43e0b
AKSHET PATIAL <akshet.patial@gmail.com>
2023-05-16 22:48:03 +0530

    Added the Link

--- src/com/akshet/HappyNumber.java
+++ src/com/akshet/HappyNumber.java
@@ -1,6 +1,12 @@
 package com.akshet;
 
+//https://leetcode.com/problems/happy-number/
+
+// Will Use the Same Concept as Fast and Slow pointer
 public class HappyNumber {
 
+    public static void main(String[] args) {
+        
+    }
 
 }
