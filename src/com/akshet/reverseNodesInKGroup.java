0706a1bb34ea99d77ec0a64b1aac6337213f0ccb
AKSHET PATIAL <akshet.patial@gmail.com>
2023-05-23 22:06:10 +0530

    Minor changes

--- src/com/akshet/reverseNodesInKGroup.java
+++ src/com/akshet/reverseNodesInKGroup.java
@@ -1,5 +1,6 @@
 package com.akshet;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 //https://leetcode.com/problems/reverse-nodes-in-k-group/
@@ -7,6 +8,86 @@ public class reverseNodesInKGroup {
 
     public static ListNode reverseKNode(ListNode head, int k){
 
+//        ListNode temp = head;
+//
+//        while(temp!=null)
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        ListNode temp = head;
+        ListNode temp1 = head;
+
+        while(temp!=null){
+            list.add(temp.val);
+            temp=temp.next;
+        }
+
+        // New Approach
+
+
+        // Pretty Bad Approach
+        /*ListNode newList = new ListNode();
+        ListNode tail = newList;
+
+        int i = 0;
+        int num = k;
+        int check = list.size()-1;
+        while(i<list.size()) {
+            if(check >= list.size()-k) {
+                if (k % 2 == 0) {
+                    while (num >= i) {
+                        if (num <= list.size()) {
+                            tail.next = new ListNode(list.get(num - 1));
+                            tail = tail.next;
+                        }
+                        num--;
+                        i++;
+                    }
+                } else {
+                    while (num >= i - 1) {
+                        if (num <= list.size()) {
+                            tail.next = new ListNode(list.get(num - 1));
+                            tail = tail.next;
+                        }
+                        num--;
+                        i++;
+                    }
+                }
+                num = k + i;
+                temp1 = temp1.next;
+                check--;
+            }else{
+                tail.next=temp1;
+            }
+        }
+
+        return newList.next;
+
+
+         */
+//        ListNode start =head;
+//        ListNode end = head;
+//        while(k!=1){
+//            end = end.next;
+//            k--;
+//        }
+       // ListNode rev = reverse(start,end);
+        return null;
+    }
+
+    private static ListNode reverse(ListNode start, ListNode end) {
+        ListNode current = start;
+        ListNode next;
+        ListNode prev = null;
+
+        while(current!=end){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        current.next =  prev;
+        return current;
     }
 
     public static void main(String[] args) {
